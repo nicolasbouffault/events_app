a7b7b3fac7a830aa486fabbb3dd479b82950849d
Nicolas Bouffault <nicolasbouffault@gmail.com>
2023-03-07 20:48:02 +0000

    initial commit

---
+++ pages/events/index.tsx
@@ -0,0 +1,23 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>Events page</h1>
+            <div>
+                <a href="">
+                    <img src="" alt="" />
+                    <h2>Events in London</h2>
+                </a>
+                <a href="">
+                    <img src="" alt="" />
+                    <h2>Events in San Francisco</h2>
+                </a>
+                <a href="">
+                    <img src="" alt="" />
+                    <h2>Events in Barcelona</h2>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Page;

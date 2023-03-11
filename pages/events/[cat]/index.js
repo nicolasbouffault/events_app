6909663f11127e90419213e59f5c51b55c50672a
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2023-03-11 12:24:53 +0000

    getStaticPaths Event Page

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -11,3 +11,26 @@ const EventsCatPage = () => {
 }
 
 export default EventsCatPage
+
+export async function getStaticPaths() {
+    const {events_categories} = await import('/data/data.json')
+    const allPaths = events_categories.map(ev => {
+        return {
+            params: {
+                cat: ev.id.toString()
+            }
+        }
+    })
+    
+    return {
+        paths: allPaths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    console.log(context)
+    return {
+        props: {}
+    }
+}

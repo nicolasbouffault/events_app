e1a64ed3a0922c946a181a4f70ead679d09277ff
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2023-03-14 20:37:29 +0000

    Work on Single Event page

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -5,3 +5,28 @@ const EventPage = () => {
 }
 
 export default EventPage
+
+export async function getStaticPaths() {
+    const {allEvents} = await import('/data/data.json')
+
+    const allPaths = allEvents.map(path => {
+        return {
+            params: {
+                cat: path.city,
+                id: path.id
+            }
+        }
+    })
+
+    return {
+        paths: allPaths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    console.log(context)
+    return {
+        props: {}
+    }
+}

780330226aa7d10970a1358b98335c0ed2d8fb7f
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2023-03-27 20:35:38 +0100

    Data for single event page

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,6 +1,12 @@
-const EventPage = () => {
+import Image from 'next/image'
+
+const EventPage = ({data}) => {
     return (
-        <h1>Event</h1>
+        <div>
+            <Image src={data.image} width={1000} height={500} alt={data.title} />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+        </div>
     )
 }
 
@@ -26,7 +32,11 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     console.log(context)
+    const id = context.params.id
+    const {allEvents} = await import('/data/data.json')
+    const eventData = allEvents.find((ev) => id === ev.id)
+
     return {
-        props: {}
+        props: {data: eventData}
     }
 }

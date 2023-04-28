1461d81e6624b29d52f2469a15a7a094c3490b3c
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2023-04-28 21:09:11 +0100

    form submit logic

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,6 +1,8 @@
 export default function handler (req, res) {
     const {method} = req
-    if(method === 'POST') {
-        
+    if(method === "POST") {
+        const { email, eventId } = req.body
+
+        res.status(200).json({ message: `You have been registered successfully with the email: ` })
     } 
 }
--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 const singleEvent = ({data}) => {
 
-  const onSubmit = () => {}
+  const inputEmail = useRef()
+  const router = useRouter()
+  
+  const onSubmit = async(e) => {
+    e.preventDefault()
+    const emailValue = inputEmail.current.value
+    const eventId = router?.query.id
+    try {
+      const response = await fetch('/api/email-registration')
+    } catch (error) {
+      console.log('ERROR', error)
+    }
+  }
 
   return (
     <div className="event_single_page">
@@ -12,7 +25,7 @@ const singleEvent = ({data}) => {
       <p>{data.description}</p>
       <form onSubmit={onSubmit} className="email_registration">
         <label> Get Registered for this event!</label>
-        <input type="email" id="email" placeholder="Please insert your email here" />
+        <input ref={inputEmail} type="email" id="email" placeholder="Please insert your email here" />
         <button type="submit">Submit</button>
       </form>
     </div>
